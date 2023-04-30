679e646a60280960146a8f4570f1a171601e329b
Anais Elisabeth <anaiselisa77@gmail.com>
2023-04-30 23:37:14 +0200

    test

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+
+const url= "http://localhost:1337/api/contacts?populate=*"
+
+
 const App = () => {
+  const [links, setLinks] = React.useState([]);
+
+    React.useEffect(() => {
+        Axios.post(url).then((response) => {
+            setLinks(response.data.data.attributes);
+        });
+    }, []);
 
     return (
       <div className="App">
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 
-
 const App = () => {
-  const [posts, setPost] = useState({});
+  const [posts, setPosts] = useState([]);
 
-  
   useEffect(() => {
-    Axios.get('http://localhost:1337/api/home?populate=*') 
+    Axios.get('http://localhost:1337/api/home?populate=*')
       .then(response => {
-        setPost(response.data.data.attributes); 
+        setPosts([response.data?.data]);
       })
       .catch(error => {
         console.error('Erreur de requête HTTP :', error);
@@ -18,11 +16,13 @@ const App = () => {
 
   return (
     <div className="App">
-        {posts.logo_background && (<img src={'http://localhost:1337' + posts.logo_background.data.attributes.url} alt="logo_background" />)}
-        <p>{posts.slogan}</p>  
-        <h1>{posts.restaurant.data.attributes.name}</h1>
-        <h1>{posts.description}</h1>
-        {posts.restaurant_picture && (<img src={'http://localhost:1337' + posts.restaurant_picture.data.attributes.url} alt="restaurant_picture" />)}
+      {posts.map((post) => (
+        <div key={post?.id}>
+          <p>{post?.attributes?.slogan}</p>
+          <h1>{post?.attributes?.restaurant?.data?.attributes?.name}</h1>
+          <h1>{post?.attributes?.description}</h1>
+        </div>
+      ))}
     </div>
   );
 };
