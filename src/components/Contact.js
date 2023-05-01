2afa442d4846d700e56e0833cab5e7ee10a1ffd8
Anais Elisabeth <anaiselisa77@gmail.com>
2023-05-01 02:53:42 +0200

    test

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,84 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Home = () => {
+const Contact = () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+    nb_of_person: '',
+    date_time: '',
+    restaurant: '',
+
+  });
+
+  useEffect(() => {
+    axios.get('http://localhost:1337/api/restaurants?')
+      .then(response => {
+        setRestaurants(response.data.data.attributes);
+        console.log(response.data.data)
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setFormData(prevState => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    axios.post('http://localhost:1337/api/contacts', formData)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
-      <h1>Bienvenue sur la page de contact !</h1>
+      <h1>Contactez-nous</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Prénom :
+          <input type="text" name="first_name" value={formData.first_name} onChange={handleChange} />
+        </label>
+        <label>
+          Nom :
+          <input type="text" name="last_name" value={formData.last_name} onChange={handleChange} />
+        </label>
+        <label>
+          Email :
+          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        </label>
+        <label>
+          Nombre de personnes :
+          <input type="number" name="nb_of_person" value={formData.nb_of_person} onChange={handleChange} />
+        </label>
+        <label>
+          Date et heure :
+          <input type="datetime-local" name="date_time" value={formData.date_time} onChange={handleChange} />
+        </label>
+        <label>
+        Restaurant :
+        <select name="restaurant" value={formData.restaurant} onChange={handleChange}>
+          <option value="">Sélectionnez un restaurant</option>
+          {restaurants && restaurants.map(restaurant => (
+          <option key={restaurant.id} value={restaurant.id}>
+          {restaurant.data.attributes.name}
+          </option>
+        ))}
+        </select>
+        </label>
+        <button type="submit">Envoyer</button>
+      </form>
     </div>
   );
 };
 
-export default Home;
+export default Contact;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,21 +6,35 @@ const url = "http://localhost:1337/api/navbar?populate=*";
 
 const Navbar = () => {
   const [links, setLinks] = React.useState([]);
+  const [logoUrl, setLogoUrl] = React.useState('');
 
   React.useEffect(() => {
-    Axios.get(url).then((response) => {
-      setLinks(response.data.data.attributes.Navbar);
-    });
+    Axios.get(url)
+      .then((response) => {
+        const data = response.data.data.attributes;
+        setLinks(data.Navbar);
+        if (response.data.data.attributes.logo) {
+          setLogoUrl(response.data.data.attributes.logo.data.attributes.formats?.medium?.url);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
 
   return (
-    <ul>
-      {links.map(link => (
-        <li key={link?.id}>
-          <Link to={link?.Slug}>{link?.Label}</Link>
-        </li>
-      ))}
-    </ul>
+    <nav>
+      <ul>
+        <div className="navbar-logo">
+          {logoUrl && <img src={logoUrl} alt="Logo" />}
+        </div>
+        {links.map(link => (
+          <li key={link?.id}>
+            <Link to={link?.Slug}>{link?.Label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
