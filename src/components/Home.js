db9ecf4997413876f9c54c3cb0639b56a7f4d07f
Anais Elisabeth <anaiselisa77@gmail.com>
2023-05-02 09:20:38 +0200

    testoo

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,6 +20,7 @@ const App = () => {
     <div className="App">
       {posts.map((post) => (
         <div key={post?.id}>
+
           <section class="banner">
             <div class="content">
               <h1>{post?.attributes?.restaurant?.data?.attributes?.name}</h1>
@@ -27,16 +28,25 @@ const App = () => {
             </div>
             <img src={`http://localhost:1337${post?.attributes?.logo_background?.data?.attributes?.url}`} alt="Logo Background" />
           </section>
+
           <section class="slogan">
             <h1>{post?.attributes?.slogan}</h1>
           </section>
 
-        
+          <section class="us">
+          <div class="image-container">
+            <img src={`http://localhost:1337${post?.attributes?.restaurant_picture?.data?.attributes?.url}`} alt="restaurant picture" />
+            </div>
+            <div class="text-container">
+            <h1>About us ... </h1>
+              <p>{post?.attributes?.description}</p>
+              <h2>Opening Hours : {post?.attributes?.restaurant_opening_hours} - {post?.attributes?.restaurant_closing_hours}</h2>
+              <h2>Opening Days : {post?.attributes?.opening_days}</h2>
+            </div>
+          </section>
+
           
-          <h1>{post?.attributes?.description}</h1>
-          <p>Opening Hours : {post?.attributes?.restaurant_opening_hours} - {post?.attributes?.restaurant_closing_hours}</p>
-          <p>Opening Days : {post?.attributes?.opening_days}</p>
-          <img src={`http://localhost:1337${post?.attributes?.restaurant_picture?.data?.attributes?.url}`} alt="restaurant picture" />
+
           {post?.attributes?.restaurants_name?.data?.map((restaurant) => (<p key={restaurant?.id}>{restaurant?.attributes?.name}</p>))}
           <h1>{post?.attributes?.restaurant?.data?.attributes?.phone_number}</h1>
           <h1>{post?.attributes?.restaurant?.data?.attributes?.restaurant_mail}</h1>
