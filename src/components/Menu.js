509870b03605ad7d759c795943966d7b9edc9e2a
Anais Elisabeth <anaiselisa77@gmail.com>
2023-05-03 15:19:07 +0200

    test

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./css/Menu.css";
 
 const Menu = () => {
   const [menuData, setMenuData] = useState(null);
@@ -20,8 +21,9 @@ const Menu = () => {
   }
 
   return (
-    <div>
+    <div className="menu">
       <h1>{menuData.Title_Drinks}</h1>
+
       <ul>
         {menuData.drinks_items.map((item) => (
           <li key={item.id}>
@@ -33,11 +35,12 @@ const Menu = () => {
       </ul>
 
       <h1>{menuData.Title_Main_courses}</h1>
+
       <ul>
         {menuData.main_courses_items.map((item) => (
           <li key={item.id}>
             <h2>{item.title}</h2>
-            <p>{item.description}</p>
+            <p >{item.description}</p>
             <p>{item.price}$</p>
           </li>
         ))}
